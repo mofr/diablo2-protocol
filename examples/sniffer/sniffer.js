c13aa2b24b4d7ac52edbd369188446f137c712c6
mofr <mofr@zond.org>
2019-11-03 21:55:25 +0700

    Rewrite sniffer from pcap to cap in order to run it on windows

--- examples/sniffer/sniffer.js
+++ examples/sniffer/sniffer.js
@@ -8,46 +8,63 @@ if (process.argv.length !== 4) {
 // If the version correspond to a supported version else use default
 const version = supportedVersions.find(v => v === process.argv[3]) ? process.argv[3] : defaultVersion
 
+const Parser = require('protodef').Parser
+const networkInterface = process.argv[2]
+
+var Cap = require('cap').Cap;
+var decoders = require('cap').decoders;
+var PROTOCOL = decoders.PROTOCOL;
+
+var capture = new Cap();
+var device = Cap.findDevice(networkInterface);
+var filter = 'tcp';
+var bufSize = 10 * 1024 * 1024;
+var buffer = Buffer.alloc(65535);
+var linkType = capture.open(device, filter, bufSize, buffer);
+capture.setMinBytes && capture.setMinBytes(0);
+
 /*
 in a new chrome tab press f12 then do this :
 const ws = new WebSocket('ws://localhost:8080')
 
 ws.onmessage = (message) => console.log(message.data)
  */
-
-const Parser = require('protodef').Parser
-const networkInterface = process.argv[2]
-
-const pcap = require('pcap')
-
-const tcpTracker = new pcap.TCPTracker()
-
-const WebSocket = require('ws')
-
-const wss = new WebSocket.Server({ port: 8080 })
-
-// Broadcast to all.
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
 wss.broadcast = function broadcast (data) {
   wss.clients.forEach(function each (client) {
     if (client.readyState === WebSocket.OPEN) {
       client.send(data)
     }
   })
-}
+};
 
-const pcapSession = pcap.createSession(networkInterface, 'ip proto \\tcp')
+capture.on('packet', function (nbytes, trunc) {
+  if (linkType === 'ETHERNET') {
+    var ret = decoders.Ethernet(buffer);
 
-pcapSession.on('packet', function (rawPacket) {
-  const packet = pcap.decode.packet(rawPacket)
-  tcpTracker.track_packet(packet)
-})
+    if (ret.info.type === PROTOCOL.ETHERNET.IPV4) {
+      ret = decoders.IPV4(buffer, ret.offset);
+      if (ret.info.protocol === PROTOCOL.IP.TCP) {
+        var datalen = ret.info.totallen - ret.hdrlen;
+        ret = decoders.TCP(buffer, ret.offset);
+        var srcPort = ret.info.srcport;
+        var dstPort = ret.info.dstport;
+        datalen -= ret.hdrlen;
+        data = buffer.slice(ret.offset, ret.offset + datalen);
+        if (trackedPorts.has(srcPort) || trackedPorts.has(dstPort)) {
+          if (datalen > 0) {
+            // console.log("\nPacket from " + srcPort + " to " + dstPort + ", " + datalen + " bytes");
+            parsePacket(data, srcPort, dstPort);
+          }
+        }
+      }
+    }
+  }
+});
 
-const FullPacketParser = require('protodef').Parser
-const ProtoDef = require('protodef').ProtoDef
-
-const express = require('express')
-const app = express()
-// app.use(express.static(`${__dirname}/public`))
+const FullPacketParser = require('protodef').Parser;
+const ProtoDef = require('protodef').ProtoDef;
 
 const {
   protocol,
@@ -56,102 +73,103 @@ const {
   d2gsReader,
   itemParser,
   bitfieldLE
-} = require('../..')
+} = require('../..');
 
-const mcpToServer = new ProtoDef(false)
-mcpToServer.addProtocol(protocol[version].mcp, ['toServer'])
+const mcpToServer = new ProtoDef(false);
+mcpToServer.addProtocol(protocol[version].mcp, ['toServer']);
 
-const mcpToClient = new ProtoDef(false)
-mcpToClient.addProtocol(protocol[version].mcp, ['toClient'])
+const mcpToClient = new ProtoDef(false);
+mcpToClient.addProtocol(protocol[version].mcp, ['toClient']);
 
-const sidToServer = new ProtoDef(false)
-sidToServer.addProtocol(protocol[version].sid, ['toServer'])
+const sidToServer = new ProtoDef(false);
+sidToServer.addProtocol(protocol[version].sid, ['toServer']);
 
-const sidToClient = new ProtoDef(false)
-sidToClient.addProtocol(protocol[version].sid, ['toClient'])
+const sidToClient = new ProtoDef(false);
+sidToClient.addProtocol(protocol[version].sid, ['toClient']);
 
-const bnftpToServer = new ProtoDef(false)
-bnftpToServer.addProtocol(protocol[version].bnftp, ['toServer'])
+const bnftpToServer = new ProtoDef(false);
+bnftpToServer.addProtocol(protocol[version].bnftp, ['toServer']);
 
-const bnftpToClient = new ProtoDef(false)
-bnftpToClient.addProtocol(protocol['1.13'].bnftp, ['toClient'])
+const bnftpToClient = new ProtoDef(false);
+bnftpToClient.addProtocol(protocol['1.13'].bnftp, ['toClient']);
 
-const d2gsToClient = new ProtoDef(false)
-d2gsToClient.addTypes(d2gsReader)
-d2gsToClient.addTypes(bitfieldLE)
-d2gsToClient.addProtocol(protocol['1.13'].d2gs, ['toClient'])
+const d2gsToClient = new ProtoDef(false);
+d2gsToClient.addTypes(d2gsReader);
+d2gsToClient.addTypes(bitfieldLE);
+d2gsToClient.addProtocol(protocol['1.13'].d2gs, ['toClient']);
 
-const d2gsToServer = new ProtoDef(false)
-d2gsToServer.addProtocol(protocol[version].d2gs, ['toServer'])
+const d2gsToServer = new ProtoDef(false);
+d2gsToServer.addProtocol(protocol[version].d2gs, ['toServer']);
 
-const toClientParser = new FullPacketParser(d2gsToClient, 'packet')
-const splitter = createSplitter()
-splitter.sloppyMode = true
-let messagesToClient = []
-let messagesToServer = []
+const toClientParser = new FullPacketParser(d2gsToClient, 'packet');
+const splitter = createSplitter();
+splitter.sloppyMode = true;
+let messagesToClient = [];
+let messagesToServer = [];
 
 splitter.on('data', data => {
   const uncompressedData = decompress(data)
 
   toClientParser.write(uncompressedData)
-})
+});
 
 toClientParser.on('data', ({ data, buffer }) => {
   try {
-    let { name, params } = data
+    let { name, params } = data;
 
     if (name === 'D2GS_ITEMACTIONWORLD' || name === 'D2GS_ITEMACTIONOWNED') {
       params = itemParser(buffer)
     }
-    wss.broadcast(JSON.stringify({ protocol: 'd2gsToClient', name, params }))
-    console.info('d2gsToClient : ', name, JSON.stringify(params))
+    wss.broadcast(JSON.stringify({ protocol: 'd2gsToClient', name, params }));
+    console.info('d2gsToClient : ', name, JSON.stringify(params));
     // console.log('raw', 'd2gsToClient', name, buffer)
     messagesToClient.push(`d2gsToClient : ${name} ${JSON.stringify(params)}`)
   } catch (err) {
-    console.log(err)
+    console.log(err);
     console.log('raw', 'd2gsToClient', buffer)
   }
-})
+});
 
-let clientPortSid = null
-let clientPortBnFtp = null
-let compression = false
+let clientPortSid = null;
+let clientPortBnFtp = null;
+let compression = false;
 
 // server ports
-const sidPort = '6112'
-const d2gsPort = '4000'
-let mcpPort = '6113'
-let mcpIp = null
+const sidPort = 6112;
+const d2gsPort = 4000;
+let mcpPort = 6113;
+let mcpIp = null;
 
-const trackedPorts = new Set([sidPort, d2gsPort, mcpPort])
+const trackedPorts = new Set([sidPort, d2gsPort, mcpPort]);
 
 function displayD2gsToClient (data) {
   try {
     if (!compression) {
-      if (data[0] !== 0xaf) { data = data.slice(1) }
+      // if (data[0] !== 0xaf) { data = data.slice(1) }
 
-      const parsed = d2gsToClient.parsePacketBuffer('packet', data).data
+      const parsed = d2gsToClient.parsePacketBuffer('packet', data).data;
 
-      const { name, params } = parsed
-      wss.broadcast(JSON.stringify({ protocol: 'd2gsToClient', name, params }))
-      console.info('d2gsToClient (uncompressed): ', name, JSON.stringify(params))
+      const { name, params } = parsed;
+      wss.broadcast(JSON.stringify({ protocol: 'd2gsToClient', name, params }));
+      console.info('d2gsToClient (uncompressed): ', name, JSON.stringify(params));
       if (name === 'D2GS_NEGOTIATECOMPRESSION' && params.compressionMode !== 0) {
-        console.log('enable compression')
+        console.log('enable compression');
         compression = true
       }
     } else {
       splitter.write(data)
     }
   } catch (error) {
-    console.log('d2gsToClient : ', error.message)
+    console.log('d2gsToClient : ', error.message);
     console.log('raw', 'd2gsToClient', data)
   }
 }
 
 function displayParsed (proto, protoName, data, raw = false) {
   try {
-    const { name, params } = proto.parsePacketBuffer('packet', data).data
-    console.log(protoName, ':', name, JSON.stringify(params))
+    const { name, params } = proto.parsePacketBuffer('packet', data).data;
+    console.log(protoName, ':', name, JSON.stringify(params));
+    return { name, params };
     wss.broadcast(JSON.stringify({ protocol: protoName, name, params }))
     if (raw) console.log('raw', protoName, name, data.toString('hex'))
     messagesToServer.push(`${protoName}:${name} ${JSON.stringify(params)}`)
@@ -211,67 +229,37 @@ function displayBnftpToClient (data) {
   }
 }
 
-const challengeParserServer = new Parser(bnftpToServer, 'CHALLENGE')
-challengeParserServer.on('error', err => console.log('bnftpToServer bnftp error : ', err.message))
+const challengeParserServer = new Parser(bnftpToServer, 'CHALLENGE');
+challengeParserServer.on('error', err => console.log('bnftpToServer bnftp error : ', err.message));
 challengeParserServer.on('data', (parsed) => {
   console.info('bnftpToServer challenge : ', JSON.stringify(parsed))
-})
-const protocolParserServer = new Parser(bnftpToServer, 'FILE_TRANSFER_PROTOCOL')
-protocolParserServer.on('error', err => console.log('bnftpToServer bnftp error : ', err.message))
+});
+const protocolParserServer = new Parser(bnftpToServer, 'FILE_TRANSFER_PROTOCOL');
+protocolParserServer.on('error', err => console.log('bnftpToServer bnftp error : ', err.message));
 protocolParserServer.on('data', (parsed) => {
   console.info('bnftpToServer protocol : ', JSON.stringify(parsed))
-})
+});
 function displayBnftpToServer (data) {
   try {
     protocolParserServer.write('FILE_TRANSFER_PROTOCOL')
     // console.log('bnftpToServer protocol: ', JSON.stringify(bnftpToServer.parsePacketBuffer('FILE_TRANSFER_PROTOCOL', data).data))
     // console.log('bnftpToServer protocol: ', data)
   } catch (error) {
-    console.log('bnftpToServer error: ', error)
+    console.log('bnftpToServer error: ', error);
     console.log('bnftpToServer write challenge', data)
     // challengeParserServer.write(data)
   }
 }
 
-// tracker emits sessions, and sessions emit data
-tcpTracker.on('session', function (session) {
-  const srcPort = session.src_name.split(':')[1]
-  const dstPort = session.dst_name.split(':')[1]
-  const srcIp = session.src_name.split(':')[0]
-  const dstIp = session.dst_name.split(':')[0]
-  if (!trackedPorts.has(srcPort) && !trackedPorts.has(dstPort)) {
-    return
+function parsePacket(data, srcPort, dstPort) {
+  if (trackedPorts.has(srcPort)) {
+    onSessionDataRecv(data);
+  }
+  else if (trackedPorts.has(dstPort)) {
+    onSessionDataSend(data);
   }
 
-  /*
-  if (dstPort === sidPort) {
-    if (clientPortSid === null) {
-      console.log('zalloooo')
-      clientPortSid = srcPort
-    } else {
-      clientPortBnFtp = srcPort
-    }
-  }
-  if (srcPort === sidPort) {
-    if (clientPortSid === null) {
-      clientPortSid = dstPort
-    } else {
-      clientPortBnFtp = dstPort
-    }
-  } */
-
-  session.on('start', function () {
-    if (srcPort === d2gsPort || dstPort === d2gsPort) {
-      console.log('Start of d2gs session')
-    }
-    if ((srcPort === mcpPort || dstPort === mcpPort) && (dstIp === mcpIp || srcIp === mcpIp || mcpIp === null)) {
-      console.log('Start of mcp session')
-    }
-    if ((srcPort === sidPort || dstPort === sidPort) && ((dstIp !== mcpIp && srcIp !== mcpIp) || mcpIp === null)) {
-      console.log('Start of sid session')
-    }
-  })
-  session.on('data send', function (session, data) {
+  function onSessionDataSend(data) {
     if (srcPort === d2gsPort) {
       displayD2gsToClient(data)
     }
@@ -280,31 +268,31 @@ tcpTracker.on('session', function (session) {
       displayD2gsToServer(data)
     }
 
-    if (srcPort === mcpPort && (srcIp === mcpIp || mcpIp === null)) {
+    if (srcPort === mcpPort) {
       displayMcpToClient(data)
     }
 
-    if (dstPort === mcpPort && (dstIp === mcpIp || mcpIp === null)) {
+    if (dstPort === mcpPort) {
       displayMcpToServer(data)
     }
 
     if (dstPort === sidPort && data.length === 1 && data[0] === 1) {
-      console.log(`sid on port ${srcPort} : ${data}`)
-      clientPortSid = srcPort
+      console.log(`sid on port ${srcPort} : ${data}`);
+      clientPortSid = srcPort;
       return
     }
 
     if (dstPort === sidPort && data.length === 1 && data[0] === 2) {
-      console.log(`bnftp on port ${srcPort} : ${data}`)
-      clientPortBnFtp = srcPort
+      console.log(`bnftp on port ${srcPort} : ${data}`);
+      clientPortBnFtp = srcPort;
       return
     }
 
-    if (srcPort === sidPort && dstPort === clientPortSid && (srcIp !== mcpIp || mcpIp === null)) {
+    if (srcPort === sidPort && dstPort === clientPortSid) {
       displaySidToClient(data)
     }
 
-    if (dstPort === sidPort && srcPort === clientPortSid && (dstIp !== mcpIp || mcpIp === null)) {
+    if (dstPort === sidPort && srcPort === clientPortSid) {
       displaySidToServer(data)
     }
 
@@ -315,41 +303,41 @@ tcpTracker.on('session', function (session) {
     if (dstPort === sidPort && srcPort === clientPortBnFtp) {
       displayBnftpToServer(data)
     }
-  })
-  session.on('data recv', function (session_, data) {
+  }
+  function onSessionDataRecv(data) {
     if (srcPort === d2gsPort) {
-      displayD2gsToServer(data)
+      displayD2gsToClient(data)
     }
 
     if (dstPort === d2gsPort) {
       displayD2gsToClient(data)
     }
 
-    if (srcPort === mcpPort && (srcIp === mcpIp || mcpIp === null)) {
+    if (srcPort === mcpPort) {
       displayMcpToServer(data)
     }
 
-    if (dstPort === mcpPort && (dstIp === mcpIp || mcpIp === null)) {
+    if (dstPort === mcpPort) {
       displayMcpToClient(data)
     }
 
     if (srcPort === sidPort && data.length === 1 && data[0] === 1) {
-      console.log(`sid on port ${dstPort} : ${data}`)
-      clientPortSid = dstPort
+      console.log(`sid on port ${dstPort} : ${data}`);
+      clientPortSid = dstPort;
       return
     }
 
     if (srcPort === sidPort && data.length === 1 && data[0] === 2) {
-      console.log(`bnftp on port ${dstPort} : ${data}`)
-      clientPortBnFtp = dstPort
+      console.log(`bnftp on port ${dstPort} : ${data}`);
+      clientPortBnFtp = dstPort;
       return
     }
 
-    if (srcPort === sidPort && dstPort === clientPortSid && (srcIp !== mcpIp || mcpIp === null)) {
+    if (srcPort === sidPort && dstPort === clientPortSid) {
       displaySidToServer(data)
     }
 
-    if (dstPort === sidPort && srcPort === clientPortSid && (dstIp !== mcpIp || mcpIp === null)) {
+    if (dstPort === sidPort && srcPort === clientPortSid) {
       displaySidToClient(data)
     }
 
@@ -360,27 +348,16 @@ tcpTracker.on('session', function (session) {
     if (dstPort === sidPort && srcPort === clientPortBnFtp) {
       displayBnftpToClient(data)
     }
-  })
+  }
+}
 
-  session.on('end', function () {
-    if (srcPort === d2gsPort || dstPort === d2gsPort) {
-      console.log('End of d2gs session')
-    }
-    if ((srcPort === mcpPort || dstPort === mcpPort) && (dstIp === mcpIp || srcIp === mcpIp || mcpIp === null)) {
-      console.log('End of mcp session')
-    }
-    if ((srcPort === sidPort || dstPort === sidPort) && (dstIp === mcpIp || srcIp === mcpIp || mcpIp === null)) {
-      console.log('End of sid session')
-    }
-  })
-})
+console.log('loaded');
 
-console.log('loaded')
-
-// Set view engine to use, in this case 'pug'
-app.set('view engine', 'pug')
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Sniffer', messagesToClient: messagesToClient, messagesToServer: messagesToServer })
-})
-app.listen(process.env.PORT || 3001)
+});
+app.listen(process.env.PORT || 3001);
